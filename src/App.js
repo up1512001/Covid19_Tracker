bd3b4097fee82ac102890fa143ba1b781d6b6040
up1512001 <utsav.patel1215@outlook.com>
2022-06-09 13:19:49 +0530

    completed api request for getting countries list in header

--- src/App.js
+++ src/App.js
@@ -1,15 +1,37 @@
-import { useState } from 'react';
+import { useState , useEffect } from 'react';
 import React from 'react';
 import { FormControl, MenuItem, Select } from '@material-ui/core';
 import './App.css';
 
 function App() {
+  
+  // state is variable in react or short term memory
 
-  const [countries,setCountries] = useState([
-    'India' , 'USA' , 'France'
-  ]);
+  const [countries,setCountries] = useState([]);
 
-// state is variable in react or short term memory
+  // useEffect run's peace of code on given conditiion
+  useEffect(()=>{
+    // empty brackte mean's componenet run's once when app is loaded not again
+    // we need to do async call because it will call server and wait for some time then gets info
+
+    const getCountriesData = async() =>{
+      await fetch('https://disease.sh/v3/covid-19/countries')
+      .then((response) =>response.json())
+      .then((data) =>{
+        const countries = data.map((country) =>(
+          {
+            name : country.country,
+            value : country.countryInfo.iso2
+          }
+        ));
+        setCountries(countries);
+      });
+    }
+
+    getCountriesData();
+
+  },[]);
+
   return (
     // BEM naming convention
     <div className="app">
@@ -29,7 +51,7 @@ function App() {
             
               {
                 countries.map((country) =>(
-                  <MenuItem value={country} >{country}</MenuItem>
+                  <MenuItem value={country.value} >{country.name}</MenuItem>
                 ))
               }
 
