2f26e728221e9bcf4e6f3aa461f13235ab81bd04
up1512001 <utsav.patel1215@outlook.com>
2022-06-10 13:41:53 +0530

    basic layout completed

--- src/App.js
+++ src/App.js
@@ -1,64 +1,70 @@
-import { useState , useEffect } from 'react';
+import { useState, useEffect } from 'react';
 import React from 'react';
 import { FormControl, MenuItem, Select } from '@material-ui/core';
 import './App.css';
 import InfoBox from './InfoBox';
+import Map from './Map'
 
+import {Card , CardContent , Typography} from '@material-ui/core';
 
 function App() {
-  
+
   // state is variable in react or short term memory
 
-  const [countries,setCountries] = useState([]);
+  const [countries, setCountries] = useState([]);
 
-  const [country,setCountry] = useState('worldwide');
+  const [country, setCountry] = useState('worldwide');
 
   // useEffect run's peace of code on given conditiion
-  useEffect(()=>{
+  useEffect(() => {
     // empty brackte mean's componenet run's once when app is loaded not again
     // we need to do async call because it will call server and wait for some time then gets info
 
-    const getCountriesData = async() =>{
+    const getCountriesData = async () => {
       await fetch('https://disease.sh/v3/covid-19/countries')
-      .then((response) =>response.json())
-      .then((data) =>{
-        const countries = data.map((country) =>(
-          {
-            name : country.country,
-            value : country.countryInfo.iso2
-          }
-        ));
-        setCountries(countries);
-      });
+        .then((response) => response.json())
+        .then((data) => {
+          const countries = data.map((country) => (
+            {
+              name: country.country,
+              value: country.countryInfo.iso2
+            }
+          ));
+          setCountries(countries);
+        });
     }
 
     getCountriesData();
 
-  },[]);
+  }, []);
 
 
-  const onCountryChange = async (event) =>{
+  const onCountryChange = async (event) => {
     const countryCode = event.target.value;
-    
+
     setCountry(countryCode);
   }
 
   return (
     // BEM naming convention
     <div className="app">
+      <div className='app__left'>
         <div className='app__header'>
+          {/* Header */}
+          {/* Title + select input dropdown list */}
+
           <h1>COVID-19 Tracker</h1>
 
           <FormControl className='app__dropdown'>
             <Select
               variant='outlined'
               value={country}
-              onChange = {onCountryChange}              
+              onChange={onCountryChange}
             >
               {/* loop through contries show dropdown list and also add worldwide */}
               <MenuItem value='worldwide'>Worldwide</MenuItem>
               {
-                countries.map((country) =>(
+                countries.map((country) => (
                   <MenuItem value={country.value} >{country.name}</MenuItem>
                 ))
               }
@@ -66,29 +72,36 @@ function App() {
 
             </Select>
 
-        </FormControl>
+          </FormControl>
+        </div>
+
+        {/* InfoBoxs */}
+        {/* InfoBoxs */}
+        {/* InfoBoxs */}
+
+        <div className='app__stats'>
+
+          <InfoBox title="Coronavirus cases" cases={129} total={2000} />
+          <InfoBox title="Recovered" cases={134} total={300} />
+          <InfoBox title="Deaths" cases={149} total={40} />
+
+        </div>
+        {/* Map */}
+        <Map />
+
       </div>
-
-      <div className='app__stats'>
-
-        <InfoBox title="Coronavirus cases" cases={129} total={2000} />
-        <InfoBox title="Recovered" cases={134} total={300} />
-        <InfoBox title="Deaths" cases={149} total={40} />
-
-      </div>
-
-
-        {/* Header */}
-        {/* Title + select input dropdown list */}
-
-        {/* InfoBoxs */}
-        {/* InfoBoxs */}
-        {/* InfoBoxs */}
+      
+      <Card className='app__right' >
 
         {/* Table of contries based on covid cases */}
         {/* Graph */}
+        <CardContent>
+              <h3>Live Cases By Country</h3>
 
-        {/* Map */}
+              <h3>Worldwide New Cases</h3>
+        </CardContent>
+
+      </Card>
 
     </div>
   );
---
+++ src/Map.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Map() {
+  return (
+    <h1 className='map'>
+        I am a map.
+    </h1>
+  );
+}
+
+export default Map
